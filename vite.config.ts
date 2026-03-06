4e8f0ca11d2c2e7c98730bab641f2e53f691e280
Thomas Kiefer <74317464+thomaskiefer@users.noreply.github.com>
2026-03-07 00:13:29 +0100

    chore: add local env and deployment tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: {
     port: 3000,
   },
+  esbuild: {
+    jsx: 'automatic',
+  },
   plugins: [
-    tailwindcss(),
     tsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tanstackStart(),
+    nitro({ preset: 'vercel' }),
     viteReact(),
+    tailwindcss(),
   ],
 })
